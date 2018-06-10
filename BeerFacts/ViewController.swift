cd7eab7c805213a89fda7e329022e432ae4e449e
Jesse Johnston <jesse.johnston@gmail.com>
2018-06-10 13:12:29 -0700

    Creating project

---
+++ BeerFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeerFacts
+//
+//  Created by Jesse Johnston on 6/10/18.
+//  Copyright © 2018 Jesse Johnston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
