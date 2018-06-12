c2478dec767b630b64534ccdf32a1ce3792b1ed8
Jesse Johnston <jesse.johnston@gmail.com>
2018-06-11 20:42:07 -0700

    Created quick spec

---
+++ BeerFacts/ViewControllerSpec.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+import Quick
+import Nimble
+
+class ViewControllerSpec: QuickSpec {
+    override func spec() {
+        describe("ViewController") {
+            
+            it("should be setup") {
+                expect(true).to(be(true))
+            }
+
+        }
+    }
+}
