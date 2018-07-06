90cc8a6b4c1f6b8e9c62180a27f48f96abbfb64f
Jesse Johnston <jesse.johnston@gmail.com>
2018-07-06 12:58:46 -0700

    Finished Beer Spec

--- BeerFacts/Services/Test/BeerSpec.swift
+++ BeerFacts/Services/Test/BeerSpec.swift
@@ -21,8 +21,22 @@ class BeerSpec: QuickSpec {
                     expect(subject.count).to(equal(3))
                     
                     expect(subject[0].name).to(equal("Buzz"))
+                    expect(subject[0].tagline).to(equal("A Real Bitter Experience."))
+                    expect(subject[0].description).to(equal("A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."))
+                    expect(subject[0].abv).to(equal(4.5))
+                    expect(subject[0].ibu).to(equal(60))
                     
-                    //TODO: Finish me
+                    expect(subject[1].name).to(equal("Trashy Blonde"))
+                    expect(subject[1].tagline).to(equal("You Know You Shouldn't"))
+                    expect(subject[1].description).to(equal("A titillating, neurotic, peroxide punk of a Pale Ale. Combining attitude, style, substance, and a little bit of low self esteem for good measure; what would your mother say? The seductive lure of the sassy passion fruit hop proves too much to resist. All that is even before we get onto the fact that there are no additives, preservatives, pasteurization or strings attached. All wrapped up with the customary BrewDog bite and imaginative twist."))
+                    expect(subject[1].abv).to(equal(4.1))
+                    expect(subject[1].ibu).to(equal(41.5))
+                    
+                    expect(subject[2].name).to(equal("Berliner Weisse With Yuzu - B-Sides"))
+                    expect(subject[2].tagline).to(equal("Japanese Citrus Berliner Weisse."))
+                    expect(subject[2].description).to(equal("Japanese citrus fruit intensifies the sour nature of this German classic."))
+                    expect(subject[2].abv).to(equal(4.2))
+                    expect(subject[2].ibu).to(beNil())
                 }
                 
             }
--- BeerFacts/TestAppDelegate.swift
+++ BeerFacts/TestAppDelegate.swift
@@ -14,15 +14,6 @@ class TestAppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = UIViewController()
         window?.makeKeyAndVisible()
         
-//        let assemblies: [Assembly] = [MainAssembly()]
-//
-//        assembler = Assembler(assemblies, container: SwinjectStoryboard.defaultContainer)
-//
-//        // Instantiate the root view controller with dependencies injected by the container.
-//
-//        let storyboard = SwinjectStoryboard.create(name: "BeerList", bundle: nil, container: SwinjectStoryboard.defaultContainer)
-//        window.rootViewController = storyboard.instantiateInitialViewController()
-        
         return true
     }
     
